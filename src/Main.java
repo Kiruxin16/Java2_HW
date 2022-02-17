f005363adcf6beaeffcf569cbd8a462abee4cd27
Kirill <kiruxin16@gmail.com>
2022-02-18 02:10:18 +0700

    Задание Java 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 //import lesson1.*;
 //import lesson2.*;
 import lesson3.*;
+import lesson5.Methods;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("   dfs");
+
+        Methods.monoThreat(); //1047,1074,1070,1056
+
+        Methods.multyThreat(); //503,502,509,493
+
 
 
     }
+
+
 
 
 
